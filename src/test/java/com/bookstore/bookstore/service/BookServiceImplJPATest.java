64ed9a54d6b3b6a644c5277b6199079dde89f960
Sai Kumar <saikumar.peddaboina@zemosolabs.com>
2022-01-10 10:00:21 +0530

    Code Coverage 86.5%

--- src/test/java/com/bookstore/bookstore/service/BookServiceImplJPATest.java
+++ src/test/java/com/bookstore/bookstore/service/BookServiceImplJPATest.java
@@ -1,7 +1,6 @@
 package com.bookstore.bookstore.service;
 
 import com.bookstore.bookstore.entity.Book;
-import com.bookstore.bookstore.exception.MyException;
 import com.bookstore.bookstore.repository.BookRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,21 +12,20 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class BookServiceImplJPATest {
+    @Autowired
+    BookServiceJPA bookServiceJPA;
+    @MockBean
+    BookRepository bookRepository;
 
-        @Autowired
-        BookServiceJPA bookServiceJPA;
-        @MockBean
-        BookRepository bookRepository;
     @Test
     void save() {
-        Book book=new Book(1,"HARRYPOTTER",200.5,123);
+        Book book = new Book(1, "HARRYPOTTER", 200.5, 123);
         bookServiceJPA.save(book);
-        verify(bookRepository,times(1)).save(book);
+        verify(bookRepository, times(1)).save(book);
     }
 
     @Test
@@ -36,16 +34,6 @@ class BookServiceImplJPATest {
         Optional<Book> userById = Optional.of(book);
         when(bookRepository.findById(1)).thenReturn(userById);
         assertEquals(bookServiceJPA.findById(1), book);
-    }
-
-    @Test
-    void findById_exceptionTest() {
-        Exception exception = assertThrows(MyException.class, () -> {
-            bookServiceJPA.findById(2);
-        });
-        String expectedMessage = "Did not find Book with id - 2";
-        String actualMessage = exception.getMessage();
-        assertEquals(expectedMessage, actualMessage);
     }
 
     @Test
@@ -59,36 +47,6 @@ class BookServiceImplJPATest {
 
     @Test
     void deleteById() {
-        bookServiceJPA.deleteById(1);
-        verify(bookRepository, times(1)).deleteById(1);
-    }
-
-    @Test
-    void testSave() {
-        Book book = new Book(1, "HARRYPOTTER", 200.5, 123);
-        bookServiceJPA.save(book);
-        verify(bookRepository, times(1)).save(book);
-    }
-
-    @Test
-    void testFindById() {
-        Book book = new Book(1, "HARRYPOTTER", 200.5, 123);
-        Optional<Book> userById = Optional.of(book);
-        when(bookRepository.findById(1)).thenReturn(userById);
-        assertEquals(bookServiceJPA.findById(1), book);
-    }
-
-    @Test
-    void testFindAll() {
-        when(bookRepository.findAll()).thenReturn(Stream.of(
-                new Book(1, "HARRYPOTTER", 200.5, 123),
-                new Book(2, "HARRYPOTTER1", 2002.5, 3123)
-        ).collect(Collectors.toList()));
-        assertEquals(2, bookServiceJPA.findAll().size());
-    }
-
-    @Test
-    void testDeleteById() {
         bookServiceJPA.deleteById(1);
         verify(bookRepository, times(1)).deleteById(1);
     }
